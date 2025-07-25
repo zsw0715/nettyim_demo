bc88afd018eed7284caa14a3b94cf1ca83dda81b
莫塔里小土豆 <shenweizhang0715@outlook.com>
2025-07-24 19:12:03 -0500

    jdk serializer

--- src/main/java/com/example/nettyim_demo/netty/protocol/Serializer.java
+++ src/main/java/com/example/nettyim_demo/netty/protocol/Serializer.java
@@ -1,14 +1,25 @@
 package com.example.nettyim_demo.netty.protocol;
 
+/**
+ * 序列化接口定义
+ * 目前只实现了 Java 原生序列化，未来可以扩展为 JSON 来实现前后端连接。
+ */
 public interface Serializer {
-
+    /**
+     * 序列化和反序列化的接口定义
+     * 提供通用的序列化和反序列化方法，支持不同的序列化算法。
+     */
     // 序列化：对象 -> 字节数组
     <T> byte[] serialize(T object);
 
     // 反序列化：字节数组 -> 对象
     <T> T deserialize(Class<T> clazz, byte[] bytes);
 
-    // 定义支持的算法类型
+
+    /**
+     * 枚举实现（Java原生序列化）
+     * 目前仅实现了 Java 原生序列化，未来可以扩展为 JSON
+     */
     enum Algorithm implements Serializer {
         // 使用 JDK 原生序列化
         JAVA {
@@ -34,6 +45,17 @@ public interface Serializer {
             }
         }
 
-        // 未来还可以拓展：JSON、Protobuf、Hessian
     }
 }
+
+
+// 序列化：对象 -> 字节
+// ByteArrayOutputStream out = new ByteArrayOutputStream();
+// ObjectOutputStream oos = new ObjectOutputStream(out);
+// oos.writeObject(object);
+// return out.toByteArray();
+
+// 反序列化：字节 -> 对象
+// ByteArrayInputStream in = new ByteArrayInputStream(bytes);
+// ObjectInputStream ois = new ObjectInputStream(in);
+// return (T) ois.readObject();
